7d44f61f35a37b577aa1014e76e6f3c2bd778790
romulo-dev <romulosr2012@hotmail.com>
2025-04-27 15:07:18 -0300

    create backend

---
+++ backend/listagem-cursos/listagem-cursos/src/main/java/com/example/listagem_cursos/ListagemCursosApplication.java
@@ -0,0 +1,13 @@
+package com.example.listagem_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListagemCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListagemCursosApplication.class, args);
+	}
+
+}
---
+++ backend/listagem-cursos/listagem-cursos/src/test/java/com/example/listagem_cursos/ListagemCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listagem_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListagemCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
